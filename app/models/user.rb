6888dd12f33dc557ce850f2ec30b24c15b4f4e6a
nichohelmut <nicholasutikal@gmail.com>
2018-12-05 18:50:21 +0100

    adds users model, controller, views, devise, active storage and basic routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many_attached :photos
+
+  def thumbnail input
+    return self.photos[input].variant(resize: '300x300!').processed
+  end
 end
